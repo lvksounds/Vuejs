f3813c119b36723393e15bfd2c900bf8563f47e7
lucas.freitas <lvksounds@gmail.com>
2022-07-05 18:50:52 -0300

    testando data biding

---
+++ testeapp/vue-starter-project/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <page-body />
+</template>
+
+<script>
+import pageBody from './components/pageBody.vue'
+
+export default {
+  name: 'App',
+  components: {
+    pageBody,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ testeapp/vue-starter-project/src/components/pageBody.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="hello">
+    <p>{{ input_name }}</p>
+    <input type="text" v-model="input_name" placeholder="Insira se nome aqui">
+    <input type="submit" value="Enviar" v-on:click="submitForm">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'pageBody',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      name: "",
+      input_name:"" 
+    }
+  },
+  methods: {
+    submitForm(e) {
+      e.preventDefault();
+
+      this.name = this.input_name
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
